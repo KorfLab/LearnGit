243f72d2cb0ad056292d007c9ca9ed096c1c3fc2
Paul Lott <plott@ucdavis.edu>
2012-09-17 12:05:48 -0700

    Moved subroutines from getNCBI.pm to getAccession.pl

--- scripts/getAccessions.pl
+++ scripts/getAccessions.pl
@@ -2,7 +2,7 @@
 use strict;
 use warnings;
 use File::Basename;
-use getNCBI;
+use LWP::Simple;
 
 die "Usage: $0 <FASTA Filename> <Genbank Filename> [NCBI accessions]" unless @ARGV>=3;
 
@@ -31,4 +31,73 @@ foreach my $acc (@ARGV){
     else{
         print "Couldn't get $acc GENBANK record from NCBI\n";
     }
+}
+
+################ Subroutines ##########################
+
+#Get NCBI Genbank file
+#Query NCBI given an accession.
+#Then returns a Genbank as scalar
+sub getNCBIGenbank{
+    my $acc = shift;
+    
+    my $accn = $acc . "[accn]";
+    
+    #assemble the esearch URL
+    my $base = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/';
+    my $url = $base . "esearch.fcgi?db=nucleotide&term=$accn&usehistory=n";
+    
+    #post the esearch URL
+    my $output = get($url);
+    
+    #parse WebEnv and QueryKey
+    my $web = $1 if ($output =~ /<WebEnv>(\S+)<\/WebEnv>/);
+    my $key = $1 if ($output =~ /<QueryKey>(\d+)<\/QueryKey>/);
+    
+    #assemble the efetch URL
+    $url = $base . "efetch.fcgi?db=nucleotide&query_key=$key&WebEnv=$web";
+    $url .= "&rettype=gb&retmode=text";
+    
+    #post the efetch URL
+    my $genbank = get($url);
+    
+    if ($genbank=~/\<\?xml/){
+        return;
+    }
+    
+    return $genbank;
+}
+
+
+#Get NCBI FASTA file
+#Query NCBI given an accession.
+#Then returns a FASTA as scalar
+sub getNCBIFasta{
+    my $acc = shift;
+    
+    my $accn = $acc . "[accn]";
+    
+    #assemble the esearch URL
+    my $base = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/';
+    my $url = $base . "esearch.fcgi?db=nucleotide&term=$accn&usehistory=n";
+    
+    #post the esearch URL
+    my $output = get($url);
+    
+    #parse WebEnv and QueryKey
+    my $web = $1 if ($output =~ /<WebEnv>(\S+)<\/WebEnv>/);
+    my $key = $1 if ($output =~ /<QueryKey>(\d+)<\/QueryKey>/);
+    
+    #assemble the efetch URL
+    $url = $base . "efetch.fcgi?db=nucleotide&query_key=$key&WebEnv=$web";
+    $url .= "&rettype=fasta&retmode=text";
+    
+    #post the efetch URL
+    my $fasta = get($url);
+    
+    if ($fasta=~/^</){
+        return;
+    }
+    
+    return $fasta;
 }
