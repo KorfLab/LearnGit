bba4cb0b259e724bf7cdc5a701531af76d3dfc28
Paul Lott <plott@ucdavis.edu>
2012-09-18 12:07:34 -0700

    Added description to buggy_script.pl

---
+++ scripts/buggy_script.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+#This script has one bug.  Fix the bug and make sure that you get the correct
+#answer.   Commit the fix.   Then create a new bug.   Commit that bug and Push
+#it to Github
+
