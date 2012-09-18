badc89ad60b5b341aff5b21da1fc3a146c7a26db
Paul Lott <plott@ucdavis.edu>
2012-09-18 12:39:33 -0700

    Created small script with a single bug

--- scripts/buggy_script.pl
+++ scripts/buggy_script.pl
@@ -6,3 +6,40 @@ use strict;
 #answer.   Commit the fix.   Then create a new bug.   Commit that bug and Push
 #it to Github
 
+print "Usage: $0 \n This script is pretty much useless" unless @ARGV==0;
+
+
+my $array_ref=fillArray();  #get reference to 4x4 array with numbers 1-16
+                            #Also prints the rows pushed onto the array
+
+#Print the rows from $array_ref
+print "After filled in:\n";
+
+for(my $row=0;$row<4;$row++){
+    my @row= @{$array_ref->[$row]};
+    my $line=join "\t", @row;
+    print $line . "\n";
+}
+
+
+#creates an 4x4 array with numbers from 1 to 16.
+#returns an array-reference to the array
+sub fillArray{
+    my @array;
+    my @row;
+    
+    print "Filling in array\n";
+    
+    for(my $i=0;$i<16;$i+=4){
+        @row=($i+1,$i+2,$i+3,$i+4);  #fill row with value
+        
+        my $line=join "\t" , @row; #join the row - just for printing on next line
+        print $line . "\n";  #print the row
+        
+        push @array, \@row;  #push row onto array
+    }
+    
+    print "\n";
+    
+    return \@array;  #return reference to @array
+}
