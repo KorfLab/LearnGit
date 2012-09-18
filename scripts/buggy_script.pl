929f6bd450f43a163ea9e4d59eb7b3ce1dceaef5
Paul Lott <plott@ucdavis.edu>
2012-09-18 13:17:13 -0700

    Final buggy_script.pl with one error.  Same as used in Perl Debug Challenge

--- scripts/buggy_script.pl
+++ scripts/buggy_script.pl
@@ -2,44 +2,26 @@
 use warnings;
 use strict;
 
-#This script has one bug.  Fix the bug and make sure that you get the correct
-#answer.   Commit the fix.   Then create a new bug.   Commit that bug and Push
-#it to Github
+#Fill table with 4 rows, numbers 1-16
+#Prints the table while adding and after adding rows
 
-print "Usage: $0 \n This script is pretty much useless" unless @ARGV==0;
+my @row;
+my @array;
 
+print "Filling in array\n";   
 
-my $array_ref=fillArray();  #get reference to 4x4 array with numbers 1-16
-                            #Also prints the rows pushed onto the array
+for(my $i=0;$i<16;$i+=4){
+    @row=($i+1,$i+2,$i+3,$i+4);  #fill row with value
+    push @array, \@row;  #push row onto array
+    my $line=join "\t" , @row; #join the row - just for printing on next line
+    print $line . "\n";  #print the row
+}
 
 #Print the rows from $array_ref
 print "After filled in:\n";
 
-for(my $row=0;$row<4;$row++){
-    my @row= @{$array_ref->[$row]};
+for(my $row_number=0;$row_number<4;$row_number++){
+    @row= @{$array[$row_number]};
     my $line=join "\t", @row;
     print $line . "\n";
-}
-
-
-#creates an 4x4 array with numbers from 1 to 16.
-#returns an array-reference to the array
-sub fillArray{
-    my @array;
-    my @row;
-    
-    print "Filling in array\n";
-    
-    for(my $i=0;$i<16;$i+=4){
-        @row=($i+1,$i+2,$i+3,$i+4);  #fill row with value
-        
-        my $line=join "\t" , @row; #join the row - just for printing on next line
-        print $line . "\n";  #print the row
-        
-        push @array, \@row;  #push row onto array
-    }
-    
-    print "\n";
-    
-    return \@array;  #return reference to @array
 }
