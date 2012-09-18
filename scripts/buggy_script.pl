4ff63fa33c1de25e67cc3c7086839247de207352
Paul Lott <plott@ucdavis.edu>
2012-09-18 15:08:35 -0700

    Changed script to calculate mean, variance, and std dev of array

--- scripts/buggy_script.pl
+++ scripts/buggy_script.pl
@@ -2,26 +2,38 @@
 use warnings;
 use strict;
 
-#Fill table with 4 rows, numbers 1-16
-#Prints the table while adding and after adding rows
+#Fill table with numbers 1-16
+#Calculates mean, population variance and std deviation of array
 
 my @row;
 my @array;
 
-print "Filling in array\n";   
+print "Filling array with 1..16\n";   
 
 for(my $i=0;$i<16;$i+=4){
-    @row=($i+1,$i+2,$i+3,$i+4);  #fill row with value
-    push @array, \@row;  #push row onto array
-    my $line=join "\t" , @row; #join the row - just for printing on next line
-    print $line . "\n";  #print the row
+    push @row , ($i+1,$i+2,$i+3,$i+4);  #fill row with value
+    push @array, @row;  #push row onto array
 }
 
-#Print the rows from $array_ref
-print "After filled in:\n";
+#Calculate Mean and Population variance and std deviation in One Pass
+my $sum = 0;
+my $square_sum = 0;
+my $n = scalar @array;
 
-for(my $row_number=0;$row_number<4;$row_number++){
-    @row= @{$array[$row_number]};
-    my $line=join "\t", @row;
-    print $line . "\n";
+for(my $i=0; $i< scalar @array; $i++){
+    $sum += $array[$i];
+    $square_sum+=$array[$i] * $array[$i];
 }
+
+my $mean = $sum/$n;
+my $variance = $square_sum/$n - $mean * $mean;
+my $sd = sqrt($variance);
+
+(abs($mean-8.5) < 0.0001) ? printf "Mean:\t%f\n",$mean :
+    printf "Incorrect Mean:\t%f\n", $mean;
+
+(abs($variance - 21.25) < 0.0001) ? printf "Population Variance:\t%f\n", $variance :
+    printf "Incorrect Variance:\t%f\n",$variance;
+
+(abs($sd - 4.609772)<0.0001) ? printf "Population Std. Deviation:\t%f\n", $sd :
+    printf "Incorrect Std Dev:\t%f\n",$sd;
