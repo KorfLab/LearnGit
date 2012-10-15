a941f29cb4e7cf0a07d5b702e75aadda19d90b34
Keith Bradnam <kbradnam@gmail.com>
2012-10-15 11:30:46 -0700

    New subroutine extract_subsequence() to extract a sequence range from a larger sequence. Will warn if trying to exceed sequence length

--- scripts/sequence_example.pl
+++ scripts/sequence_example.pl
@@ -72,6 +72,7 @@ my $rand_custom = rand_seq(1000, "custom", \%cust_ref);
 print "\n\nFunction rand_seq(\$length, \$type[dna rna protein custom], [NA or \$custom hash]\n";
 print "dna; $rand_dna\nrna: $rand_rna\npro: $rand_pro\ncustom: $rand_custom\n";
 
+
 #----------------------- Translate DNA seq to Protein ------------------------#
 # Translate a sequence and reverse translate it
 my $dnatoprotein = translate_codon($rand_dna);
@@ -79,11 +80,13 @@ my $proteintodna = rev_translate_codon($dnatoprotein);
 print "\n\nFunction translate_codon(\$dna): Translate a sequence\nFunction rev_translate_codon(\$protein): Reverse translate protein seq\n";
 print "dna:$rand_dna\ndna to protein: $dnatoprotein\nprotein to dna: $proteintodna\n\n";
 
+
 #----------------------- Shannon Entropy ------------------------#
 my $entropy = entropy_shannon($rand_dna);
 my $entropy_case = entropy_shannon($rand_dna, "case");
 print "\n\nFunction entropy_shannon(\$dna, [case]): Calculate Shannon's Entropy of a dna sequence\n";
 print "Shannon Entropy of sequence rand_dna():\ncase insensitive: $entropy\ncase sensitive: $entropy_case\n";
+
 
 #----------------------- Check for Whitespace ------------------------#
 $seq = "AAAACCCCGGGGTTTT -AAA";
@@ -91,6 +94,7 @@ $seq = "AAAACCCCGGGGTTTT -AAA";
 my $nowhitespace_seq = clean_sequence($seq);
 print "\n\nFunction clean_sequence(\$seq): Cleans whitespace from a DNA, RNA, or protein sequence\n";
 print "\nSequence before cleaning:\n$seq\n\nSequence after cleaning:\n$nowhitespace_seq \n\n";
+
 
 #-------------------- Needleman-Wunsch Alignment ---------------------#
 my $alignseq1 = "AAATTTCCCGGG";
@@ -102,3 +106,36 @@ my ($aligned1, $aligned2) = nw_align($alignseq1, $alignseq2, $MATCH, $MISMATCH, 
 print "\n\nFunction nw_align(\$seq): Aligns two sequences using the Needleman-Wunsch Alignment\n";
 print "\nBefore Needleman-Wunsch Alignment\nSeq1:\t$alignseq1\nSeq2:\t$alignseq2\n";
 print "\nAfter Needleman-Wunsch Alignment\nAligned Seq1:\t$aligned1\nAligned Seq2:\t$aligned2\n";
+
+
+#----------------------- Extract subsequence ------------------------#
+print "\n\n";
+$seq = "TACAATCCGCCGGCGGTTTT";
+
+my ($start, $length) = (2, 4);
+print "Sequence: $seq\n";
+print "Extracting from position $start, length $length\n";
+my $subseq = extract_subsequence($seq, $start, $length);
+print "Subsequence: $subseq\n\n";
+
+# negative offset
+($start, $length) = (-5, 3);
+print "Sequence: $seq\n";
+print "Extracting from position $start, length $length\n";
+$subseq = extract_subsequence($seq, $start, $length);
+print "Subsequence: $subseq\n\n";
+
+# no length specified
+$start  = 1;
+print "Sequence: $seq\n";
+print "Extracting from position $start\n";
+$subseq = extract_subsequence($seq, $start);
+print "Subsequence: $subseq\n\n";
+
+
+# Trying to extract too much sequence
+($start, $length) = (5, 100);
+print "Sequence: $seq\n";
+print "Extracting from position $start, length $length\n";
+$subseq = extract_subsequence($seq, $start, $length);
+print "Subsequence: $subseq\n\n";
