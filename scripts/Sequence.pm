9f95c6459eca8495ead402f4fc6b1e737ea0d853
mitochi <srhartono@ucdavis.edu>
2012-10-15 11:28:03 -0700

    Sequence.pm translation codon table now cope with nucleotide ambiguities

--- scripts/Sequence.pm
+++ scripts/Sequence.pm
@@ -8,19 +8,19 @@ our @ISA = qw(Exporter);
 our @EXPORT_OK = qw();
 my %Translation = (
         'AAA' => 'K', 'AAC' => 'N', 'AAG' => 'K', 'AAT' => 'N',
-        'ACA' => 'T', 'ACC' => 'T', 'ACG' => 'T', 'ACT' => 'T',
+        'ACA' => 'T', 'ACC' => 'T', 'ACG' => 'T', 'ACT' => 'T', 'ACN' => 'T', 
         'AGA' => 'R', 'AGC' => 'S', 'AGG' => 'R', 'AGT' => 'S',
         'ATA' => 'I', 'ATC' => 'I', 'ATG' => 'M', 'ATT' => 'I',
         'CAA' => 'Q', 'CAC' => 'H', 'CAG' => 'Q', 'CAT' => 'H',
-        'CCA' => 'P', 'CCC' => 'P', 'CCG' => 'P', 'CCT' => 'P',
-        'CGA' => 'R', 'CGC' => 'R', 'CGG' => 'R', 'CGT' => 'R',
-        'CTA' => 'L', 'CTC' => 'L', 'CTG' => 'L', 'CTT' => 'L',
+        'CCA' => 'P', 'CCC' => 'P', 'CCG' => 'P', 'CCT' => 'P', 'CCN' => 'P',
+        'CGA' => 'R', 'CGC' => 'R', 'CGG' => 'R', 'CGT' => 'R', 'CGN' => 'R',
+        'CTA' => 'L', 'CTC' => 'L', 'CTG' => 'L', 'CTT' => 'L', 'CTN' => 'L',
         'GAA' => 'E', 'GAC' => 'D', 'GAG' => 'E', 'GAT' => 'D',
-        'GCA' => 'A', 'GCC' => 'A', 'GCG' => 'A', 'GCT' => 'A',
-        'GGA' => 'G', 'GGC' => 'G', 'GGG' => 'G', 'GGT' => 'G',
-        'GTA' => 'V', 'GTC' => 'V', 'GTG' => 'V', 'GTT' => 'V',
+        'GCA' => 'A', 'GCC' => 'A', 'GCG' => 'A', 'GCT' => 'A', 'GCN' => 'N',
+        'GGA' => 'G', 'GGC' => 'G', 'GGG' => 'G', 'GGT' => 'G', 'GGN' => 'G',
+        'GTA' => 'V', 'GTC' => 'V', 'GTG' => 'V', 'GTT' => 'V', 'GTN' => 'V',
         'TAA' => '*', 'TAC' => 'Y', 'TAG' => '*', 'TAT' => 'Y',
-        'TCA' => 'S', 'TCC' => 'S', 'TCG' => 'S', 'TCT' => 'S',
+        'TCA' => 'S', 'TCC' => 'S', 'TCG' => 'S', 'TCT' => 'S', 'TCN' => 'S',
         'TGA' => '*', 'TGC' => 'C', 'TGG' => 'W', 'TGT' => 'C',
         'TTA' => 'L', 'TTC' => 'F', 'TTG' => 'L', 'TTT' => 'F'
 );
