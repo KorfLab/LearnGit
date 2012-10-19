2a6010d38582a401f05b12372cfdc4e46ccfeb8e
Stella <srhartono@ucdavis.edu>
2012-10-18 17:20:07 -0700

    Added initial function translate to Sequence_OOP.pm

--- scripts/Sequence_OOP.pm
+++ scripts/Sequence_OOP.pm
@@ -3,7 +3,56 @@ use warnings;
 
 my $NUCLEOTIDE="ACGTURYSWKMBDHVN._";
 my $AMINO_ACID="ABCDEFGHIKLMNPQRSTVWXYZ.*";
-
+my %Translation = (
+        'AAA' => 'K',   'AAC' => 'N',   'AAG' => 'K',   'AAT' => 'N',
+        'AAR' => 'K',   'AAY' => 'N',   'ACA' => 'T',   'ACC' => 'T',
+        'ACG' => 'T',   'ACT' => 'T',   'ACR' => 'T',   'ACY' => 'T',
+        'ACK' => 'T',   'ACM' => 'T',   'ACW' => 'T',   'ACS' => 'T',
+        'ACB' => 'T',   'ACD' => 'T',   'ACH' => 'T',   'ACV' => 'T',
+        'ACN' => 'T',   'AGA' => 'R',   'AGC' => 'S',   'AGG' => 'R',
+        'AGT' => 'S',   'AGR' => 'R',   'AGY' => 'S',   'ATA' => 'I',
+        'ATC' => 'I',   'ATG' => 'M',   'ATT' => 'I',   'ATY' => 'I',
+        'ATM' => 'I',   'ATW' => 'I',   'ATH' => 'I',   'CAA' => 'Q',
+        'CAC' => 'H',   'CAG' => 'Q',   'CAT' => 'H',   'CAR' => 'Q',
+        'CAY' => 'H',   'CCA' => 'P',   'CCC' => 'P',   'CCG' => 'P',
+        'CCT' => 'P',   'CCR' => 'P',   'CCY' => 'P',   'CCK' => 'P',
+        'CCM' => 'P',   'CCW' => 'P',   'CCS' => 'P',   'CCB' => 'P',
+        'CCD' => 'P',   'CCH' => 'P',   'CCV' => 'P',   'CCN' => 'P',
+        'CGA' => 'R',   'CGC' => 'R',   'CGG' => 'R',   'CGT' => 'R',
+        'CGR' => 'R',   'CGY' => 'R',   'CGK' => 'R',   'CGM' => 'R',
+        'CGW' => 'R',   'CGS' => 'R',   'CGB' => 'R',   'CGD' => 'R',
+        'CGH' => 'R',   'CGV' => 'R',   'CGN' => 'R',   'CTA' => 'L',
+        'CTC' => 'L',   'CTG' => 'L',   'CTT' => 'L',   'CTR' => 'L',
+        'CTY' => 'L',   'CTK' => 'L',   'CTM' => 'L',   'CTW' => 'L',
+        'CTS' => 'L',   'CTB' => 'L',   'CTD' => 'L',   'CTH' => 'L',
+        'CTV' => 'L',   'CTN' => 'L',   'GAA' => 'E',   'GAC' => 'D',
+        'GAG' => 'E',   'GAT' => 'D',   'GAR' => 'E',   'GAY' => 'D',
+        'GCA' => 'A',   'GCC' => 'A',   'GCG' => 'A',   'GCT' => 'A',
+        'GCR' => 'A',   'GCY' => 'A',   'GCK' => 'A',   'GCM' => 'A',
+        'GCW' => 'A',   'GCS' => 'A',   'GCB' => 'A',   'GCD' => 'A',
+        'GCH' => 'A',   'GCV' => 'A',   'GCN' => 'A',   'GGA' => 'G',
+        'GGC' => 'G',   'GGG' => 'G',   'GGT' => 'G',   'GGR' => 'G',
+        'GGY' => 'G',   'GGK' => 'G',   'GGM' => 'G',   'GGW' => 'G',
+        'GGS' => 'G',   'GGB' => 'G',   'GGD' => 'G',   'GGH' => 'G',
+        'GGV' => 'G',   'GGN' => 'G',   'GTA' => 'V',   'GTC' => 'V',
+        'GTG' => 'V',   'GTT' => 'V',   'GTR' => 'V',   'GTY' => 'V',
+        'GTK' => 'V',   'GTM' => 'V',   'GTW' => 'V',   'GTS' => 'V',
+        'GTB' => 'V',   'GTD' => 'V',   'GTH' => 'V',   'GTV' => 'V',
+        'GTN' => 'V',   'TAA' => '*',   'TAC' => 'Y',   'TAG' => '*',
+        'TAT' => 'Y',   'TAR' => '*',   'TAY' => 'Y',   'TCA' => 'S',
+        'TCC' => 'S',   'TCG' => 'S',   'TCT' => 'S',   'TCR' => 'S',
+        'TCY' => 'S',   'TCK' => 'S',   'TCM' => 'S',   'TCW' => 'S',
+        'TCS' => 'S',   'TCB' => 'S',   'TCD' => 'S',   'TCH' => 'S',
+        'TCV' => 'S',   'TCN' => 'S',   'TGA' => '*',   'TGC' => 'C',
+        'TGG' => 'W',   'TGT' => 'C',   'TGY' => 'C',   'TTA' => 'L',
+        'TTC' => 'F',   'TTG' => 'L',   'TTT' => 'F',   'TTR' => 'L',
+        'TTY' => 'F',   'TRA' => '*',   'YTA' => 'L',   'YTG' => 'L',
+        'YTR' => 'L',   'MGA' => 'R',   'MGG' => 'R',   'MGR' => 'R',
+);
+my %Rev_Translation;
+foreach my $nuc (keys %Translation) {
+        $Rev_Translation{$Translation{$nuc}} = $nuc;
+}
 #Need to check passed data type in the functions to make sure it is a Sequence
 #or a compatible datatype.
 
@@ -53,7 +102,7 @@ sub complement {
 	elsif ($self->{seq_type} eq "RNA") {$tmp_seq =~ tr/AUGCYRKMBDHVaugcyrkmbdhv/UACGRYMKVHDBuacgrymkvhdb/}
     my $new_obj = new Sequence($self->{header}, $tmp_seq, $self->{seq_type});
 	return $self, $new_obj;
-    
+}    
 sub rev_comp {
 	# Returns the reverse complement of $seq as a new Sequence object
 	my $self = shift;
@@ -178,8 +227,25 @@ sub extract{
 }
 
 #Translate sequence
-sub translate{
-    
+sub translate {
+	my $self = shift;
+	if (undef $self->{seq_type} or $self->{seq_type} ne "DNA" or $self->{seq_type} ne "RNA") {
+        	warn "Translate error: sequence type must be either DNA/RNA\n";
+		return $self;
+	}
+	else {
+		my $seq = $self->{sequence};
+		$seq =~ tr/Uu/Tt/;
+		my $trans;
+		for (my $i = 0; $i < length($seq)-3; $i+=3) {
+			my $codon = substr($seq, $i, 3);
+			if (not exists $Translation{$codon}) {$trans .= $undef}
+			else                                 {$trans .= $Translation{$codon}}
+		}
+	}
+	my $new_obj = new Sequence($self->{header}, $trans, "PROTEIN");
+	return ($self, $new_obj);
+   
 }
 
 #Count given k-mer size and return hash {AAA=>10,AAC=>2...}
